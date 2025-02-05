681ffb99543c62dc6293e2e162727c06cd04b4d4
Eli Rasske <rasskeli@huhs.org>
2025-02-04 18:23:14 -0600

    Create `createCountdown` function and relocate countdown CSS to master stylesheet

--- js/countdown.js
+++ js/countdown.js
@@ -1,20 +1,22 @@
-const timer = document.getElementById("timer")
-const countDownDate = new Date("Feb 16, 2025 8:00:00").getTime();
-
-let countdown = setInterval(updateCountdown, 1000);
-
-function updateCountdown() {
-    const now = new Date().getTime();
-    const distance = countDownDate - now;
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    timer.innerText = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-    if (distance < 0) {
-        clearInterval(countdown);
-        timer.textContent = "IT'S HERE!";
+function createCountdown(date, id, endText="DONE"){
+    if (typeof date == "string"){
+        date = new Date(date)
     }
-}
+    let interval = setInterval(update, 1000);
 
-updateCountdown()
+    function update() {
+        const now = new Date().getTime();
+        const distance = date - now;
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        timer.innerText = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+        if (distance < 0) {
+            clearInterval(interval);
+            timer.textContent = endText;
+        }
+    }
+
+    update()
+}
